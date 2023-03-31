0b9bd9896ec8f97ccab644454465d14605403545
nikhil <nikhil.v@2basetechnologies.com>
2023-03-31 15:39:55 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\ShortLinkController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->action([HomeController::class, 'index']);
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
+// Route::get('/user.get_data',[UserController::class, 'get_data'])->name('get_data');
+Route::resource('users', UsersController::class);
+
+
+Route::get('/list', [ShortLinkController::class, 'index'])->name('list');
+Route::post('generate-shorten-link', [ShortLinkController::class, 'store'] )->name('generate.shorten.link.post');
+Route::get('{code}', [ShortLinkController::class, 'shortenLink'] )->name('shorten.link');
